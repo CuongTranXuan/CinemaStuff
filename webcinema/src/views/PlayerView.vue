e9454b0b03ac5adf3fa0fa50ab544c2e8a3beda4
Lê Đức Thiện <leducthien.bk@gmail.com>
2020-06-17 08:40:05 +0700

    Doggo commit

--- webcinema/src/views/PlayerView.vue
+++ webcinema/src/views/PlayerView.vue
@@ -7,47 +7,43 @@
             <figure>
               <img :src="urlImg" />
             </figure>
-            <ScoreIndicator 
-              :score="itemInfo.vote_average" 
+            <ScoreIndicator
+              :score="itemInfo.vote_average"
               size="82"
               stroke-width="5"
               stroke-color="#ff6633"
             />
             <i>Vote Average</i>
           </aside>
-            <h1>{{ itemInfo.title }}</h1>
+          <h1>{{ itemInfo.title }}</h1>
 
-            <p>
-              <span v-if="!readMore">{{ this.overviewShort}}</span>
-              <span v-if="readMore">{{itemInfo.overview}}</span>
-              <span v-if="!readMore">... </span>
-              <a class="more" @click="activeReadMore">{{buttonText}}</a>
-              </p>
+          <p>
+            <span v-if="!readMore">{{ this.overviewShort}}</span>
+            <span v-if="readMore">{{itemInfo.overview}}</span>
+            <span v-if="!readMore">...</span>
+            <a class="more" @click="activeReadMore">{{buttonText}}</a>
+          </p>
 
-            <h2 class="label">
-              <font-awesome-icon 
-                :icon="icon" 
-                size="1x" 
-                class="icon" />&nbsp;
-                {{ dateLabel }}</h2>
-            <p>{{ this.date }}</p>
-            <button class="button" @click="switchPlayer" title="play">
-                    Play
-            </button> &nbsp;
-            <button class="button button2" @click="openTrailer" title="play">
-                    Trailer
-            </button> &nbsp;
-            <div v-if="togglePlay" style="height: 800px;">
-              <video-player ref="videoPlayer" 
-                  class="vjs-custom-skin vjs-quality-selector"
-                  :options="playerOptions"
-                  @play="onPlayerPlay($event)"
-                  @ready="onPlayerReady($event)"
-                  @pause="onPlayerEnded($event)"
-                  @ended="onPlayerEnded($event)"
-                  @statechanged="playerStateChanged($event)">
-              </video-player>
-            </div>
+          <h2 class="label">
+            <font-awesome-icon :icon="icon" size="1x" class="icon" />
+            &nbsp;
+            {{ dateLabel }}
+          </h2>
+          <p>{{ this.date }}</p>
+          <button class="button" @click="switchPlayer" title="play">Play</button> &nbsp;
+          <button class="button button2" @click="openTrailer" title="play">Trailer</button> &nbsp;
+          <div v-if="togglePlay" id="doggo">
+            <video-player
+              ref="videoPlayer"
+              class="vjs-custom-skin vjs-quality-selector"
+              :options="playerOptions"
+              @play="onPlayerPlay($event)"
+              @ready="onPlayerReady($event)"
+              @pause="onPlayerEnded($event)"
+              @ended="onPlayerEnded($event)"
+              @statechanged="playerStateChanged($event)"
+            ></video-player>
+          </div>
         </template>
         <template v-else>Loading ...</template>
       </div>
@@ -56,18 +52,18 @@
 </template>
 
 <script>
-import ScoreIndicator from '@/components/ScoreIndicator';
-import VideoPlayer from '@/components/VideoPlayer.vue'
-import { mapState, mapGetters } from 'vuex';
-import dayjs from 'dayjs'
+import ScoreIndicator from "@/components/ScoreIndicator";
+import VideoPlayer from "@/components/VideoPlayer.vue";
+import { mapState, mapGetters } from "vuex";
+import dayjs from "dayjs";
 
 export default {
-  name: 'PlayerView',
-  components: { 
+  name: "PlayerView",
+  components: {
     ScoreIndicator,
-    VideoPlayer 
+    VideoPlayer
   },
-  data(){
+  data() {
     return {
       readMore: false,
       buttonText: "Read More",
@@ -80,89 +76,95 @@ export default {
           durationDisplay: true
         }
       }
-    }
+    };
   },
   computed: {
-    player () {
-      return this.$refs.videoPlayer.player
+    player() {
+      return this.$refs.videoPlayer.player;
     },
-    ...mapState(['type', 'itemInfo']),
-    ...mapGetters(['imgPath']),
+    ...mapState(["type", "itemInfo"]),
+    ...mapGetters(["imgPath"]),
     showItemInfo() {
       return !Object.keys(this.itemInfo).length ? false : true;
     },
-    overviewShort(){
-      let str = this.itemInfo.overview
-      str = str.substring(0,250)
-      return str
+    overviewShort() {
+      let str = this.itemInfo.overview;
+      str = str.substring(0, 250);
+      return str;
     },
     urlImg() {
       return this.itemInfo.poster_link != null
         ? `${this.itemInfo.poster_link}`
-        : require('@/assets/images/poster-not-available.png');
+        : require("@/assets/images/poster-not-available.png");
     },
-    date(){
-      return (this.itemInfo.year) ? dayjs(this.itemInfo.year).format('MMM D, YYYY') : ''
+    date() {
+      return this.itemInfo.year
+        ? dayjs(this.itemInfo.year).format("MMM D, YYYY")
+        : "";
     },
     dateLabel() {
-      return this.itemInfo.type == 'movie' ? 'Release date' : 'First air date';
+      return this.itemInfo.type == "movie" ? "Release date" : "First air date";
     },
-    icon(){
-      return this.itemInfo.type == 'movie' ? 'film' : 'tv';
+    icon() {
+      return this.itemInfo.type == "movie" ? "film" : "tv";
     },
-    trailer(){
-      return this.itemInfo.trailer_link
-    },
+    trailer() {
+      return this.itemInfo.trailer_link;
+    }
   },
   methods: {
     switchPlayer: function() {
       // let id = this.$route.params.id
       // this.$router.push({path: `/movies/${id}/play`})
-      this.$data.togglePlay = true
-      const video = {
-        withCredentials: false,
-        type: 'application/x-mpegurl',
-        src: this.itemInfo.video_link
+      if (!this.$data.togglePlay) {
+        this.$data.togglePlay = true;
       }
-      this.player.src(video)
       // this.player.load()
-      this.player.play()
+      // this.player.play();
     },
     openTrailer: function() {
-      let trailer = this.trailer
-      window.open(trailer,"_blank")
+      let trailer = this.trailer;
+      window.open(trailer, "_blank");
     },
     activeReadMore: function() {
-      this.$data.readMore = !this.$data.readMore
-      this.$data.buttonText = this.$data.readMore === true? "Less" : "Read More"
+      this.$data.readMore = !this.$data.readMore;
+      this.$data.buttonText =
+        this.$data.readMore === true ? "Less" : "Read More";
     },
-    onPlayerPlay () {
-      window.console.log('start play!')
+    onPlayerPlay() {
+      window.console.log("start play!");
       // AppServices.startPlay(this.itemInfo.id)
     },
-    onPlayerReady () {
-      window.console.log('player ready!')
-      this.player.play()
+    onPlayerReady() {
+      window.console.log("player ready!");
+      //Only when ready does "player" object exist
+      const video = {
+        withCredentials: false,
+        type: "application/x-mpegurl",
+        src: this.itemInfo.video_link
+      };
+      this.player.src(video);
+      this.player.play();
     },
-    onPlayerEnded () {
-      window.console.log('player ended!')
+    onPlayerEnded() {
+      window.console.log("player ended!");
       // AppServices.endPlay(this.itemInfo.id)
     },
     //listen state event
     playerStateChanged(playerCurrentState) {
-      this.$data.playerState = playerCurrentState
-      window.console.log('player current update state', this.$data.playerState)
+      this.$data.playerState = playerCurrentState;
+      window.console.log("player current update state", this.$data.playerState);
     },
-    playVideo: function (source) {
+    playVideo: function(source) {
       const video = {
         withCredentials: false,
-        type: 'application/x-mpegurl',
+        type: "application/x-mpegurl",
         src: source
-      }
+      };
       //this.player.reset() // in IE11 (mode IE10) direct usage of src() when <src> is already set, generated errors,
-      this.player.src(video)
+      this.player.src(video);
       // this.player.load()
-      this.player.play()
+      this.player.play();
     }
   }
 };
@@ -170,18 +172,23 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
+#doggo {
+  height: 800px;
+  float: center;
+  padding-top: 20vh;
+}
 .player {
-        position: absolute !important;
-        width: 70%;
-        height: 70%;
-    }
+  position: absolute !important;
+  width: 70%;
+  height: 70%;
+}
 .vjs-custom-skin {
-        height: 90% !important;
-    }
+  height: 90% !important;
+}
 .vjs-custom-skin /deep/ .video-js {
-        width: 100% !important;
-        height: 90%;
-    }
+  width: 100% !important;
+  height: 90%;
+}
 figure {
   margin: 0 0 1em;
 }
@@ -214,7 +221,7 @@ p {
   line-height: 1.4;
 }
 .button {
-  background-color: #4CAF50; /* Green */
+  background-color: #4caf50; /* Green */
   border: none;
   color: white;
   padding: 15px 32px;
@@ -223,7 +230,9 @@ p {
   display: inline-block;
   font-size: 16px;
 }
-.button2 {background-color: #f44336;} /* Red */ 
+.button2 {
+  background-color: #f44336;
+} /* Red */
 a {
   display: inline-block;
   color: $color-text-secondary;
@@ -238,7 +247,7 @@ a {
 .item_content {
   padding: 2em;
 }
-.poster{
+.poster {
   display: block;
   margin-bottom: 2em;
   text-align: center;
@@ -265,17 +274,17 @@ a {
     float: left;
     margin: 0;
     width: 33%;
-    text-align: center
+    text-align: center;
   }
-  .info{
+  .info {
     display: block;
     padding-left: 2em;
-    margin-left: 33%;    
+    margin-left: 33%;
   }
-  .control{
+  .control {
     display: block;
     padding-left: 2em;
-    margin-left: 33%;    
+    margin-left: 33%;
   }
 }
 </style>
